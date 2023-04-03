9397e65f510b711189388c25671cccba58102df7
SushilGupta710 <sushilvikgupta@gmail.com>
2023-04-04 01:24:28 +0530

    some modification

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('medify-infinite-scroll app is running!');
+    expect(page.getTitleText()).toEqual('medify app is running!');
   });
 
   afterEach(async () => {
